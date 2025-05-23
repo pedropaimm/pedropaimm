71a8d78cdd324fb9c5cd7d959e6fe7fdc982ff45
pedropaimm_ <142277137+pedropaimm@users.noreply.github.com>
2025-05-23 16:51:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+cadastro = str(input('Deseja realizar um cadastro? (1.Sim/2.Não)'))
+if cadastro == 'Sim':
+    nome = str(input('Insira seu nome: '))
+    idade = int(input('Agora insira sua idade: '))
+    ano = int(input('Em qual ano escolar você se encontra?: '))
+    print('Confirme os dados:')
+    print('Nome:', nome)
+    print('Idade:', idade)
+    print('Ano Escolar:', ano)
+    
+else:
+    ('Tenha um bom dia!')
+
